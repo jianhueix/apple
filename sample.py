eb1d99beea1b11a16dfa4697dd88b79ed509e35c
jianhuei <jianhuei@gmail.com>
2024-06-07 10:16:11 -0700

    update date diff

--- sample.py
+++ sample.py
@@ -1,2 +1,10 @@
-# Hello World program in Python
-print("Hello World!23")
+from datetime import date
+
+def days_between_dates(date1, date2):
+    return (date2 - date1).days
+
+print(days_between_dates(date(2019, 1, 1), date(2019, 1, 21)))
+print(days_between_dates(date(2019, 1, 21), date(2019, 1, 1)))
+
+
+# Output: 1
